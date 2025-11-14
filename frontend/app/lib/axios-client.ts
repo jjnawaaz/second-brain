dca0209710b98fdf363f0e6b65016987c44734d0
JUNAID NAWAZ <junaidnawaz@JUNAIDs-MacBook-Air.local>
2025-11-14 17:02:46 +0530

    (Deploy): fix deploy issues

--- frontend/app/lib/axios-client.ts
+++ frontend/app/lib/axios-client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const axiosClient = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     withCredentials: true,
     timeout: 5000
 })
