9bc115d4a42c6b0e5b537c43999b207adb03bef6
JUNAID NAWAZ <junaidnawaz@JUNAIDs-MacBook-Air.local>
2025-11-17 17:45:01 +0530

    feat(frontend)- fix footer

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <div className="mt-3 h-20 flex items-center text-xs md:text-lg font-semibold justify-center border-2 border-red-500">
+      <div className="mt-3 h-20 flex items-center text-xs md:text-lg font-semibold justify-center ">
         © 2025 Second <span className="text-gray-500 ml-1 mr-1"> Brain.</span>{" "}
         Stay organized. Stay curious.
       </div>
