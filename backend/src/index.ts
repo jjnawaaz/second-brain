b88defb4139ba043a545ad4e615900d7251078dc
JUNAID NAWAZ <junaidnawaz@JUNAIDs-MacBook-Air.local>
2025-12-19 05:32:13 +0530

    fix - cors issues

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,42 +1,44 @@
 import { ApolloServer, type BaseContext } from "@apollo/server";
 import { expressMiddleware } from "@as-integrations/express5";
-import express, { type Request, type Response } from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import 'dotenv/config'
+import express, { type Request, type Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
 import { resolvers, typeDefs } from "./graphql/index.js";
 import { authMiddleware } from "./utils/authMiddleware.js";
 
-const app = express()
+const app = express();
 
 const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers,
-    introspection: true
-})
-
-await server.start()
-
-
-app.get('/clear-cookies', (req: Request, res: Response) => {
-  res.clearCookie(process.env.COOKIE_NAME as string);
-  res.json({ message: 'Cookies cleared' });
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+  introspection: true,
 });
 
+await server.start();
 
+app.get("/clear-cookies", (req: Request, res: Response) => {
+  res.clearCookie(process.env.COOKIE_NAME as string);
+  res.json({ message: "Cookies cleared" });
+});
 
-app.use('/graphql',
-    cors({
-        origin: [process.env.FRONTEND_URL as string,process.env.FRONTEND_URL1 as string] , 
-    }),
-    express.json(),
-    cookieParser(),
-    expressMiddleware(server,{
-        context: async({req,res}:{req: Request, res: Response})=>{
-            const payload = authMiddleware(req)
-            return {req,res,payload}
-        }
-    })
-)
+app.use(
+  "/graphql",
+  cors({
+    origin: [
+      process.env.FRONTEND_URL as string,
+      process.env.FRONTEND_URL1 as string,
+    ],
+    credentials: true,
+  }),
+  express.json(),
+  cookieParser(),
+  expressMiddleware(server, {
+    context: async ({ req, res }: { req: Request; res: Response }) => {
+      const payload = authMiddleware(req);
+      return { req, res, payload };
+    },
+  })
+);
 
-app.listen(process.env.PORT,()=>console.log("Server Started"))
+app.listen(process.env.PORT, () => console.log("Server Started"));
